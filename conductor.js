4e0cd812e815f3ed9d23b3165c341bf5f1b007b2
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-02-01 09:34:05 -0400

    ouse can interact with game objects.  Still in testing..

--- conductor.js
+++ conductor.js
@@ -10,12 +10,22 @@ made aware of events that effect them, and be animated in the main loop.
 */
 
 const conductor = (function () {
+  ///-------------------------PRIVATE-----------------------------------------
   /*contains all of the things in the game, buttons, game objects, etc.*/
   var objects = {};
   var canvas = null;
   let ctx = null;
   var screenSizeX = null;
   var screenSizeY = null;
+  var running = false;    //Is the mainloop supposed to keep going?
+  var frameRate = null;
+  var backgroundColor = null;
+  var oldTime = new Date();
+  var time = new Date();
+  var delta = 0;
+  var hoveredObject = null;
+  var pressedObject = null;
+  var mouseX, mouseY, mouseDown = null;
   /**
   * Converts a cartesian coordinate to a polar coordinate.
   * @param {number} x the coordinate along the horizontal (x) axis.
@@ -46,38 +56,92 @@ const conductor = (function () {
     * @returns {Point} the cartesian coordinate after transformations applied.
     */
   function rotateTranslateTransform(polar, rotation, translation) {
-    let rotated = {"r":polar.r, "a":polar.a+rotation};
+    let rotated = { "r": polar.r, "a": polar.a + rotation };
     let point = {
-      "x":Math.cos (rotated.a)*rotated.r,
-      "y":Math.sin (rotated.a)*rotated.r
+      "x": Math.cos(rotated.a) * rotated.r,
+      "y": Math.sin(rotated.a) * rotated.r
     }
     let translatedPoint = {
-      "x":point.x + translation.x,
-      "y":point.y + translation.y
+      "x": point.x + translation.x,
+      "y": point.y + translation.y
     }
     return translatedPoint;
   }
+  /**
+   * Gets the coordinate of the center of the screen.
+   * @returns point {x,y} 
+   */
+  function screenCenter() {
+    return { "x": canvas.width / 2, "y": canvas.height / 2 };
+  }
+  /**
+   * Gets the size of the screen.
+   * @returns size {w,h} 
+   */
+  function screenSize() {
+    return { "w": canvas.width, "h": canvas.height };
+  }
+  /**
+   * Gets a point after translation by another point
+   * @param {Point} p0 the initial point
+   * @param {Point} p1 the amount to translate the initial point by
+   * @returns{Point} the translated point {x,y}
+   */
+  function translate(p0, p1) {
+    return { "x": p0.x + p1.x, "y": p0.y + p1.y };
+  }
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+  function checkMouseInteractions(){}
+  function mainLoop() {
+    delta = (_time - _oldTime) / 1000;
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+    checkMouseInteractions();
+    if (running) setTimeout(mainLoop, frameRate);
+  }
 
+  //-----------------------------PUBLIC----------------------------------------
   return {
     /**
      * Creates the canvas, attach event handlers..
      * @param {string} bgColor #RGB canvas background color.
      */
     "init": function (bgColor) {
-      let color = (bgColor) ? bgColor : '#000';
+      backgroundColor = (bgColor) ? bgColor : '#000';
       let body = document.getElementsByTagName('body')[0];
       body.style.margin = "0px";
       canvas = document.createElement('canvas');
       canvas.style.padding = "0px 0px 0px 0px";
       canvas.style.margin = "0px 0px 0px 0px";
       canvas.style.border = "0px";
-      console.log(body);
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+      canvas.onmousemove = function (e){
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+      }
+      canvas.onmousedown = function(e){
+        mouseDown = true;
+      }
+      canvas.mouseUp = function (e){
+        mouseDown = false;
+      }
+      resizeCanvas();
+      window.addEventListener("resize", resizeCanvas);
       ctx = canvas.getContext('2d');
-      ctx.fillStyle = color;
+      ctx.fillStyle = backgroundColor;
       ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
       body.appendChild(canvas);
+
+    },
+    "startloop": function (frameRateMillis) {
+      frameRate = (frameRateMillis) ? frameRateMillis : 33;
+      running = true;
+      mainLoop();
+    },
+    "stopLoop": function () {
+      running = false;
     },
     /**
      * Draws all the game objects on the canvas.
@@ -91,12 +155,13 @@ const conductor = (function () {
           ctx.beginPath();
           let firstPoint = true;
           sprite.coords.forEach(polarCoordinate => {
-            let point = rotateTranslateTransform (
+            let point = rotateTranslateTransform(
               polarCoordinate, object.getOrientation(), object.getPosition()
             );
+            point = translate(point, screenCenter());
             if (firstPoint) {
               console.log(`moveto ${point.x},${point.y}`);
-              ctx.moveTo (point.x, point.y);
+              ctx.moveTo(point.x, point.y);
               firstPoint = false;
             } else {
               console.log(`lineto ${point.x},${point.y}`);
@@ -113,15 +178,15 @@ const conductor = (function () {
      * Gets the size of the window.
      * @returns {Size} the width and height {w,h}
      */
-    "screenSize": function () {
-      return { "w": canvas.width, "h": canvas.height };
+    "getScreenSize": function () {
+      return screenSize();
     },
     /**
      * Gets the coordinate of the center of the window.
      * @returns {Point} {x,y}
      */
-    "screenCenter": function () {
-      return { "x": canvas.width / 2, "y": canvas.height / 2 };
+    "getScreenCenter": function () {
+      return screenCenter();
     },
     /** 
      * Adds a sprite to this object
--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-const go = function () {
+const setup = function () {
   conductor.init('#012');
   conductor.create('test');
-  conductor.addSpriteTo(
-    'test',
-    [0,-50,30,30,-30,30],
-    '#0f0'
-  );
-  conductor.setPositionOf(
-    'test',
-    conductor.screenCenter().x, 
-    conductor.screenCenter().y
-  );
+  conductor.addSpriteTo('test', [0, -50, 30, 30, -30, 30], '#0f0');
+  conductor.setPositionOf('test', 0, 0);
   conductor.draw();
   console.log(conductor);
   console.log(conductor.has('test'));

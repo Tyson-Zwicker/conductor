9050e08779cd2e28904e250b71f8f6564cbd4448
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-02-02 18:16:24 -0400

    added labels, sprites can be un-filled now.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const setup = function () {
   conductor.init('#012');
+  conductor.setFont (14,true);
   conductor.create('test');
-  conductor.addSpriteTo('test', [0, -50, 30, 30, -30, 30], '#0f0');
-  conductor.setPositionOf('test', 0, 0);
+  conductor.addSpriteTo('test', [0, -50, 30, 30, -30, 30], '#0f0',true);
+  conductor.setPositionOf('test', -100, 0);
+  conductor.setLabelOf ('test', 'Triangle',{"x":0,"y":0},'#fff');
+  conductor.create ('test2');
+  conductor.addSpriteTo ('test2', [-20,-20,20,-20,20,20,-20,20],'#f00',false);
+  conductor.setPositionOf ('test2',300,200);
+  conductor.setOrientationOf ('test2',Math.PI/6);
+  conductor.setLabelOf ('test2', 'Square',{"x":0,"y":30},'#00f');
   conductor.runOnce();
 }

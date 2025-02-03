39ac13ba9140158431466860a00bd397a42be46b
Tyson-Zwicker <tysonzwicker@gmail.com>
2025-02-02 21:35:39 -0400

    added test for fixed coordinates (for buttons)

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const setup = function () {
   conductor.setFont (14, true);
   
   conductor.create('test');
-  conductor.addSpriteTo('test', [0, -50, 30, 30, -30, 30], '#0f0',true);
+  conductor.addSpriteTo('test', [0, -50, 30, 30, -30, 30], '#0f0');
   conductor.setPositionOf('test', -100, 0);
   conductor.setLabelOf ('test', 'Triangle',{"x":0,"y":0},'#fff');
   
   conductor.create ('test2');
-  conductor.addSpriteTo ('test2', [-20,-20,20,-20,20,20,-20,20],'#f00',false);
+  conductor.addSpriteTo ('test2', [-20,-20,20,-20,20,20,-20,20],'#f00');
   conductor.setPositionOf ('test2',300,200);
   conductor.setOrientationOf ('test2',Math.PI/6);
   conductor.setLabelOf ('test2', 'Square',{"x":0,"y":30},'#ff0');
   
   conductor.create ('button');
+
+  //TODO: This might be easier to think of if there was a addRectangleSpriteTo (x,y,w,h)
+  //because as it is now, you have to figure out the position from based on the coordintes
+  //of the sprite, so the position has coordinates that don't match anything in the sprite coordinates
   conductor.addSpriteTo ('button', [-30,-10,30,-10,30,10,-30,10],'#0af');
-  conductor.setPositionOf ('button',0,0);
+  conductor.setPositionOf ('button',35,25,true);
   conductor.setLabelOf ('button','Buttony',{"x":0,"y":0},'#fff');
   
   conductor.runOnce();
